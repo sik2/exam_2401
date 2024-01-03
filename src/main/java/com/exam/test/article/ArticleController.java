46e6c3a0cd800076fe13597152572231bd15e191
sik2 <bickwang123@gmail.com>
2024-01-03 16:23:36 +0900

    requirements 1-3

--- src/main/java/com/exam/test/article/ArticleController.java
+++ src/main/java/com/exam/test/article/ArticleController.java
@@ -1,12 +1,18 @@
 package com.exam.test.article;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/article")
+@RequiredArgsConstructor
 @Controller
 public class ArticleController {
+    private final ArticleService articleService;
+
     @GetMapping("/list")
     public String list() {
         return "article_list";
@@ -15,5 +21,12 @@ public class ArticleController {
     @GetMapping("/create")
     public String create() {
         return "article_form";
+    }
+
+    @PostMapping("/create")
+    public String create(@RequestParam("title") String title, @RequestParam("content") String content) {
+        this.articleService.create(title, content);
+
+        return "redirect:/article/list";
     }
 }
--- src/main/java/com/exam/test/article/ArticleService.java
+++ src/main/java/com/exam/test/article/ArticleService.java
@@ -1,7 +1,19 @@
 package com.exam.test.article;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
 @Service
 public class ArticleService {
+    private final ArticleRepository articleRepository;
+    public void create(String title, String content) {
+        Article article = new Article();
+        article.setTitle(title);
+        article.setContent(content);
+        article.setCreateDate(LocalDateTime.now());
+        this.articleRepository.save(article);
+    }
 }
